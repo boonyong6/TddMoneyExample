a9542acd90abed7b14f4981447f878b1f3a3c55f
boonyong <boonyong.kang93@gmail.com>
2022-08-30 21:46:12 +0800

    Refactor Dollar interface
    - Get rid of flaws in the "Dollar" interface that causes side effects.

--- TddMoneyExample/Dollar.cs
+++ TddMoneyExample/Dollar.cs
@@ -9,9 +9,9 @@
             Amount = amount;
         }
 
-        public void Times(int multiplier)
+        public Dollar Times(int multiplier)
         {
-            Amount *= multiplier;
+            return new Dollar(Amount * multiplier);
         }
     }
 }
