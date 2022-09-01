cdd646744cfd9a43f1fcccfc977b7f6882071e71
boonyong <boonyong.kang93@gmail.com>
2022-09-01 19:50:10 +0800

    Generalize Times method
    - Reconcile Times method signature so it can be moved to base class (Money).
    - Realize there's no need for derived classes, hence remove it progressively.
    - Use factory method to remove references to derived classes.
    - Add abstract Times method temporarily in Money class.

--- TddMoneyExample/Money.cs
+++ TddMoneyExample/Money.cs
@@ -1,8 +1,20 @@
 ﻿namespace TddMoneyExample
 {
-    class Money
+    abstract class Money
     {
         protected int Amount;
+
+        public static Money Dollar(int amount)
+        {
+            return new Dollar(amount);
+        }
+
+        public static Money Franc(int amount)
+        {
+            return new Franc(amount);
+        }
+
+        public abstract Money Times(int multiplier);
 
         public override bool Equals(object? obj)
         {
